eabc77e36d2134676739e3dac74a188f0ccc55ff
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2021-10-14 12:19:37 +0700

    Update hello2.py

--- hello2.py
+++ hello2.py
@@ -1,4 +1,4 @@
 a = int(input("chieu rong"))
 b = int(input(" chieu dai "))
-print("chu vi hinh chu nhat la ",(a + b)/2)
-print("diet tich hinh chu nhat la ",a * b )
+print("chu vi hinh chu nhat la ",round((a + b)/2))
+print("diet tich hinh chu nhat la ",round(a * b))
