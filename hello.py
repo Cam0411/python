8ade3c40aa553c4565dbeed4fa754ef3afe42d0f
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2021-10-14 12:06:21 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+import datetime
+day = datetime.datetime.now()
+name = (input("ten"))
+sang = int(input("calo sang"))
+trua = int(input("calo trua"))
+chieu = int(input("calo toi"))
+trongngay = int(input("so van dong"))
+sum = sang + trua + chieu - trongngay 
+a = ("{} {} co luong calo trong ngay la {} ")
+print(a.format(day,name,sum))
---
+++ hello2.py
@@ -0,0 +1,4 @@
+a = int(input("chieu rong"))
+b = int(input(" chieu dai "))
+print("chu vi hinh chu nhat la ",(a + b)/2)
+print("diet tich hinh chu nhat la ",a * b )
